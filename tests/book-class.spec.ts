084e50053df0067aa488b8e53a184f8dbf68b233
Vicente <virucu@gmail.com>
2024-09-20 10:52:07 +0200

    test

--- tests/book-class.spec.ts
+++ tests/book-class.spec.ts
@@ -3,7 +3,13 @@ import { LoginPage } from '../page-object/login-po';
 import { BookingPage } from '../page-object/booking-po';
 
 test('Book Class', async ({ page }) => {
-    test.setTimeout(1200000);
+    // test.setTimeout(1200000);
+    test.setTimeout(80000);
+    console.log(process.env.ACTIVITY);
+    console.log(process.env.ACTIVITY_TIME);
+    console.log(process.env.GYMUSERNAME);
+    console.log(process.env.PASSWORD);
+    console.log(process.env.BASE_URL);
     const loginPage = new LoginPage(page);
     const bookingPage = new BookingPage(page);
     await loginPage.goto();
