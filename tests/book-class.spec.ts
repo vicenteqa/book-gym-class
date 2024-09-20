a1dc47f1f31b1557be72af549ffeb5df269ee672
Vicente <virucu@gmail.com>
2024-09-20 12:43:04 +0200

    fix playwground

--- tests/book-class.spec.ts
+++ tests/book-class.spec.ts
@@ -3,12 +3,7 @@ import { LoginPage } from '../page-object/login-po';
 import { BookingPage } from '../page-object/booking-po';
 
 test('Book Class', async ({ page }) => {
-    // test.setTimeout(1200000);
-    console.log(process.env.ACTIVITY);
-    console.log(process.env.ACTIVITY_TIME);
-    console.log(process.env.GYMUSERNAME);
-    console.log(process.env.PASSWORD);
-    console.log(process.env.BASE_URL);
+    test.setTimeout(1200000);
     const loginPage = new LoginPage(page);
     const bookingPage = new BookingPage(page);
     await loginPage.goto();
